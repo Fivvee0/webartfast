6633b6767c21010466c368617bf878108ba6e81d
Fivvee0 <167813307+Fivvee0@users.noreply.github.com>
2024-07-15 22:13:51 +0200

    NOW WORK I THINK

--- install/installer/app/template/docker/app.py
+++ install/installer/app/template/docker/app.py
@@ -3,24 +3,30 @@ import os
 
 app = Flask(__name__)
 
-WEBSITE_DIR = './website'
-COMPONENTS_DIR = './components'
+WEBSITE_DIR = '../website'
+COMPONENTS_DIR = '../components'
 
 @app.route('/')
 def home():
-    return send_from_directory(WEBSITE_DIR, 'index.html')
+    try:
+        return send_from_directory(WEBSITE_DIR, 'index.html')
+    except Exception as e:
+        return abort(404, description="Home page not found")
 
 @app.route('/<path:filename>')
 def serve_file(filename):
-    website_path = os.path.join(WEBSITE_DIR, filename)
-    if os.path.isfile(website_path):
-        return send_from_directory(WEBSITE_DIR, filename)
-    
-    components_path = os.path.join(COMPONENTS_DIR, filename)
-    if os.path.isfile(components_path):
-        return send_from_directory(COMPONENTS_DIR, filename)
+    try:
+        website_path = os.path.join(WEBSITE_DIR, filename)
+        if os.path.isfile(website_path):
+            return send_from_directory(WEBSITE_DIR, filename)
+        
+        components_path = os.path.join(COMPONENTS_DIR, filename)
+        if os.path.isfile(components_path):
+            return send_from_directory(COMPONENTS_DIR, filename)
 
-    return abort(404)
+        return abort(404, description="File not found")
+    except Exception as e:
+        return abort(500, description="Internal Server Error")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
