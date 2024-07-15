f7b13d9d159354f17b6eed9e4adadb5557ef4c02
Fivvee0 <167813307+Fivvee0@users.noreply.github.com>
2024-07-15 21:06:23 +0200

    ...

--- example/python/app.py
+++ example/python/app.py
@@ -3,8 +3,8 @@ import os
 
 app = Flask(__name__)
 
-WEBSITE_DIR = '../website'
-COMPONENTS_DIR = '../components'
+WEBSITE_DIR = os.path.join(os.getcwd(), 'website')
+COMPONENTS_DIR = os.path.join(os.getcwd(), 'components')
 
 @app.route('/')
 def home():
--- source-code/app/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd ../python
-
-pip install --no-cache-dir -r requirements.txt
-python app.py
-
-echo Done!
