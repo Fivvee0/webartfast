e4e0fcb2bf33183261ff735ed887faf92054e635
Fivvee0 <167813307+Fivvee0@users.noreply.github.com>
2024-07-15 20:57:45 +0200

    again...

--- example/docker/app.py
+++ example/docker/app.py
@@ -1,30 +1,26 @@
-import argparse
+from flask import Flask, send_from_directory, abort
 import os
-import shutil
 
-def main():
-    parser = argparse.ArgumentParser(description='Tool for creating web apps.')
-    subparsers = parser.add_subparsers(dest='command')
+app = Flask(__name__)
 
-    create_parser = subparsers.add_parser('create', help='Creates a new web app.')
-    create_parser.add_argument('--name', required=True, help='Name of app.')
-    create_parser.add_argument('--dir', required=True, help='Directory.')
+WEBSITE_DIR = '../website'
+COMPONENTS_DIR = '../components'
 
-    args = parser.parse_args()
+@app.route('/')
+def home():
+    return send_from_directory(WEBSITE_DIR, 'index.html')
 
-    if args.command == 'create':
-        script_dir = os.path.dirname(os.path.abspath(__file__))
-        template_dir = os.path.join(script_dir, 'template')
+@app.route('/<path:filename>')
+def serve_file(filename):
+    website_path = os.path.join(WEBSITE_DIR, filename)
+    if os.path.isfile(website_path):
+        return send_from_directory(WEBSITE_DIR, filename)
+    
+    components_path = os.path.join(COMPONENTS_DIR, filename)
+    if os.path.isfile(components_path):
+        return send_from_directory(COMPONENTS_DIR, filename)
 
-        create_website(args.name, args.dir, template_dir)
-
-def create_website(name, output_dir, template_dir):
-    app_dir = os.path.join(output_dir, name)
-    os.makedirs(app_dir, exist_ok=True)
-
-    shutil.copytree(template_dir, app_dir, dirs_exist_ok=True)
-
-    print(f'Created Web App: {name} in directory: {app_dir}.')
+    return abort(404)
 
 if __name__ == '__main__':
-    main()
+    app.run(host='0.0.0.0', port=80)
